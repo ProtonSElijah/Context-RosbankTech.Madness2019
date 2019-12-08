a67752158224c0a932db9e7f0b836157adba6abb
Georgii Timofeevskii <gtimoshaz@gmail.com>
2019-12-08 06:12:36 +0300

    Теперь работает отправка на сервер

--- mobile-app/FoolBar/app/src/main/java/com/example/foolbar/SupportChat.java
+++ mobile-app/FoolBar/app/src/main/java/com/example/foolbar/SupportChat.java
@@ -3,6 +3,7 @@ package com.example.foolbar;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
@@ -10,11 +11,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SupportChat extends AppCompatActivity {
-
+    String clientProblemText;
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +37,7 @@ public class SupportChat extends AppCompatActivity {
         long appfail = sharedPreferences.getLong("application_fail", 0);
         long inact_click = sharedPreferences.getLong("last_inactive_click", 0);
 
-        ArrayList<String> clientProblems = new ArrayList<>();
+        final ArrayList<String> clientProblems = new ArrayList<>();
         boolean anyProblem = false;
 
         Log.d("chat_currenttime", String.valueOf(currentTime));
@@ -43,10 +54,37 @@ public class SupportChat extends AppCompatActivity {
 
         TextView tv = findViewById(R.id.support_message_init);
         if (anyProblem) {
-            String clientProblemText = "Наш оператор уже знает, что у вас " + String.join(" и ", clientProblems) + ". Он уже работает над вашей проблемой";
+
+            clientProblemText = "Наш оператор уже знает, что у вас " + String.join(" и ", clientProblems) + ". Он уже работает над вашей проблемой";
             tv.setText(clientProblemText);
         } else {
-            tv.setText("Расскажите о своей проблеме");
+            clientProblemText = "Расскажите о своей проблеме";
+            tv.setText(clientProblemText);
         }
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://192.168.43.76:8080/newError";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("network-response", response);
+                    }
+                },
+                new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("network-error", error.getMessage());
+            }
+        }) {
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                Log.d("network", "getBody");
+                return clientProblemText.getBytes();
+            }
+        };
+
+        queue.add(postRequest);
+
     }
 }
